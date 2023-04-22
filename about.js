1b2091bbda48cc59c16becf224ce93d9459c148c
Maboela Maserame <maboelamaserame@gmail.com>
2023-04-22 16:38:41 +0200

    Add linters

--- about.js
+++ about.js
@@ -1,13 +1,13 @@
 // Toggle menu when hamburger icon is clicked
-document.querySelector(".hamburger").addEventListener("click", function () {
-  this.classList.toggle("active");
-  document.querySelector(".dropdown-menu").classList.toggle("active");
+document.querySelector('.hamburger').addEventListener('click', function () {
+  this.classList.toggle('active');
+  document.querySelector('.dropdown-menu').classList.toggle('active');
 });
 
 // Close menu when a dropdown menu link is clicked
-document.querySelectorAll(".dropdown-menu a").forEach(function (link) {
-  link.addEventListener("click", function () {
-    document.querySelector(".hamburger").classList.remove("active");
-    document.querySelector(".dropdown-menu").classList.remove("active");
+document.querySelectorAll('.dropdown-menu a').forEach((link) => {
+  link.addEventListener('click', () => {
+    document.querySelector('.hamburger').classList.remove('active');
+    document.querySelector('.dropdown-menu').classList.remove('active');
   });
 });
--- index.js
+++ index.js
@@ -1,59 +1,59 @@
 const datas = [
   {
-    Image: "../Images/bass-guitarist.jpg",
-    name: "Kenya Malaika",
-    pronounce: "She",
-    description: "bassist ",
-    major: "jazz and classical",
-    experience: "composer,song writer,vocal arranger and music director",
+    Image: '../Images/bass-guitarist.jpg',
+    name: 'Kenya Malaika',
+    pronounce: 'She',
+    description: 'bassist ',
+    major: 'jazz and classical',
+    experience: 'composer,song writer,vocal arranger and music director',
   },
   {
-    Image: "../Images/percussion.jpg",
-    name: "Azania Culo",
-    pronounce: "He",
-    description: "Pianist",
-    major: "contemporary Jazz",
-    experience: "music director, arranger and composer",
+    Image: '../Images/percussion.jpg',
+    name: 'Azania Culo',
+    pronounce: 'He',
+    description: 'Pianist',
+    major: 'contemporary Jazz',
+    experience: 'music director, arranger and composer',
   },
   {
-    Image: "../Images/guitarist.jpg",
-    name: "Themba Africa",
-    pronounce: "He",
-    description: "Guitarist",
-    major: "contemporary Jazz",
-    experience: "composer,song writer and vocal coach",
+    Image: '../Images/guitarist.jpg',
+    name: 'Themba Africa',
+    pronounce: 'He',
+    description: 'Guitarist',
+    major: 'contemporary Jazz',
+    experience: 'composer,song writer and vocal coach',
   },
   {
-    Image: "../Images/drummer.jpg",
-    name: "T-zone zuco",
-    pronounce: "He",
-    description: "drummer",
-    major: "jazz and classical",
-    experience: "composer,song writer and arraner",
+    Image: '../Images/drummer.jpg',
+    name: 'T-zone zuco',
+    pronounce: 'He',
+    description: 'drummer',
+    major: 'jazz and classical',
+    experience: 'composer,song writer and arraner',
   },
   {
-    Image: "../Images/Singer.jpg",
-    name: "Koketso Moloko",
-    pronounce: "She",
-    description: "vocalist",
-    major: "jazz and funk",
-    experience: "composer,song writer and music director",
+    Image: '../Images/Singer.jpg',
+    name: 'Koketso Moloko',
+    pronounce: 'She',
+    description: 'vocalist',
+    major: 'jazz and funk',
+    experience: 'composer,song writer and music director',
   },
   {
-    Image: "../Images/percs.jpg",
-    name: "Zakes Percs",
-    pronounce: "Percussionist",
-    description: "Guitarist",
-    major: "jazz and african jazz",
-    experience: "composer,song writer and sound engineer",
+    Image: '../Images/percs.jpg',
+    name: 'Zakes Percs',
+    pronounce: 'Percussionist',
+    description: 'Guitarist',
+    major: 'jazz and african jazz',
+    experience: 'composer,song writer and sound engineer',
   },
 ];
 
-const postContainer = document.querySelector('.dynamic')
+const postContainer = document.querySelector('.dynamic');
 
-const postMethods = () =>{
-  datas.map((postData)=>{
-    const postElement=document.createElement('div');
+const postMethods = () => {
+  datas.map((postData) => {
+    const postElement = document.createElement('div');
     postElement.classList.add('dynamic');
     postElement.innerHTML = `
       <div class= 'cards'>
@@ -68,24 +68,20 @@ const postMethods = () =>{
     `;
 
     postContainer.appendChild(postElement);
-  })
-
+  });
+};
+postMethods();
+function toggleMenu() {
+  document.querySelector('.hamburger').classList.toggle('active');
+  document.querySelector('.dropdown-menu').classList.toggle('active');
 }
-postMethods()
- function toggleMenu() {
-   document.querySelector(".hamburger").classList.toggle("active");
-   document.querySelector(".dropdown-menu").classList.toggle("active");
- }
 
- function closeMenu() {
-   document.querySelector(".hamburger").classList.remove("active");
-   document.querySelector(".dropdown-menu").classList.remove("active");
- }
- document.querySelectorAll(".dropdown-menu a").forEach(function (link) {
-   link.addEventListener("click", function () {
-     closeMenu();
-   });
- });
-
- 
-
+function closeMenu() {
+  document.querySelector('.hamburger').classList.remove('active');
+  document.querySelector('.dropdown-menu').classList.remove('active');
+}
+document.querySelectorAll('.dropdown-menu a').forEach((link) => {
+  link.addEventListener('click', () => {
+    closeMenu();
+  });
+});
